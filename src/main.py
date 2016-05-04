ae300ed2ccc43c514a74c7b216a963276b5deda5
Michael Gilliland <mjg.py3@gmail.com>
2016-05-03 21:09:03 -0400

    Dummy evaluator and main

--- src/main.py
+++ src/main.py
@@ -1,3 +1,13 @@
 #!/usr/bin/env python
 
-print "not much here yet"
+from sys import argv
+
+from evaluator import evaluate
+from parser import parse
+from sugar import desugar
+from tokenizer import tokenize
+
+if __name__ == '__main__':
+  code = argv[1]
+
+  print evaluate(desugar(parse(tokenize(code))))
